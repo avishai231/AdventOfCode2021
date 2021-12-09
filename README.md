c3c800c6488ceafb2edf0f2d74208ac6075075c6
avishai231 <avishai231@gmail.com>
2021-12-09 11:37:36 +0200

    finished day 2b

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AdventOfCode2021
+My take on the 2021 Advent of Code Christmas challenge
--- day2/day2.py
+++ day2/day2.py
@@ -5,15 +5,35 @@ def read_file(filename):
     return(commands)
 
 def update_pos(command_list):
-    depth = 0; horiz = 0
+    depth = 0; horiz = 0; aim = 0
     for i in command_list:
         if i[0] == 'forward': horiz += i[1]
         if i[0] == 'up': depth += i[1]
         if i[0] == 'down': depth -= i[1]
     return abs(depth)*horiz
+
+def update_pos2(command_list):
+    depth = 0; horiz = 0; aim = 0
+    for i in command_list:
+        if i[0] == 'forward':
+            horiz += i[1]
+            depth += aim*i[1]
+        if i[0] == 'up':
+            aim -= i[1]
+        if i[0] == 'down':
+            aim += i[1]
+    return abs(depth)*horiz
+
+
+
+
+
 if __name__ == '__main__':
     #print(read_file('test.txt'))
     final_pos = update_pos(read_file('test.txt'))
     assert final_pos == 150, 'got ' + str(final_pos) + ' expected 150'
+    final_pos2 = update_pos2(read_file('test.txt'))
+    assert final_pos2 == 900, 'got ' + str(final_pos) + ' expected 900'
     print(update_pos(read_file('input.txt')))
+    print(update_pos2(read_file('input.txt')))
 
